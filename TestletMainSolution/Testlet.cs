7a319fb4c3058923a0899becf9c4c4e7966c8e9d
ravimanirajan <ravimanirajan@gmail.com>
2022-09-29 11:22:37 +0530

    Testlet application

---
+++ TestletMainSolution/Testlet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestletMainSolution
+{
+    public class Testlet
+    {
+        public string TestletId;
+        private List<Item> Items;
+
+        public Testlet(string testletId, List<Item> items)
+        {
+            TestletId = testletId;
+            Items = items;
+        }
+        public List<Item> Randomize()
+        {
+            List<Item> randomitems = new List<Item>();
+            if (Items != null && Items.Any())
+            {
+                var pretestItems = Items.Where(e => e.ItemType == ItemTypeEnum.Pretest).OrderBy(p => Guid.NewGuid()).Take(2).ToList();
+                randomitems.AddRange(pretestItems);
+
+                var allItems = Items.Where(e => !pretestItems.Any(p => p.ItemId == e.ItemId)).OrderBy(a => Guid.NewGuid());
+                randomitems.AddRange(allItems);
+            }
+            Console.WriteLine(randomitems);
+            return randomitems;
+        }
+    }
+    public class Item
+    {
+        public string ItemId;
+        public ItemTypeEnum ItemType;
+    }
+    public enum ItemTypeEnum
+    {
+        Pretest = 0,
+        Operational = 1
+    }
+}
---
+++ TestletUnittest/TestLetUnittest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestletMainSolution;
+
+namespace TestletUnittest
+{
+    [TestClass]
+    public class TestLetUnittest
+    {
+        List<Item> items = new List<Item>();
+
+        [TestInitialize]
+        public void Testinitialize()
+        {
+            items.Add(new Item() { ItemId = "1", ItemType = ItemTypeEnum.Operational });
+            items.Add(new Item() { ItemId = "2", ItemType = ItemTypeEnum.Operational });
+            items.Add(new Item() { ItemId = "3", ItemType = ItemTypeEnum.Operational });
+            items.Add(new Item() { ItemId = "4", ItemType = ItemTypeEnum.Operational });
+            items.Add(new Item() { ItemId = "5", ItemType = ItemTypeEnum.Operational });
+            items.Add(new Item() { ItemId = "6", ItemType = ItemTypeEnum.Operational });
+            items.Add(new Item() { ItemId = "7", ItemType = ItemTypeEnum.Pretest });
+            items.Add(new Item() { ItemId = "8", ItemType = ItemTypeEnum.Pretest });
+            items.Add(new Item() { ItemId = "9", ItemType = ItemTypeEnum.Pretest });
+            items.Add(new Item() { ItemId = "10", ItemType = ItemTypeEnum.Pretest });
+        }
+        [TestMethod]
+        public void TestRandomWithFirstTwoPretest()
+        {
+            Testlet testlet = new Testlet("1", items);
+            var randomItems = testlet.Randomize();
+            Assert.IsNotNull(randomItems);
+            Assert.IsTrue(randomItems.Count == 10);
+            Assert.IsTrue(randomItems.Take(2).All(e => e.ItemType == ItemTypeEnum.Pretest));
+        }
+
+        [TestMethod]
+        public void TestRandomWithAll()
+        {
+            Testlet testlet = new Testlet("1", items);
+            var randomItems = testlet.Randomize();
+            Assert.IsNotNull(randomItems);
+            Assert.IsTrue(randomItems.Count == 10);
+            Assert.IsTrue(randomItems.Skip(2).All(e => e.ItemType == ItemTypeEnum.Pretest || e.ItemType == ItemTypeEnum.Operational));
+        }
+
+        [TestMethod]
+        public void TestRandomWithOrderItems()
+        {
+            Testlet testlet = new Testlet("1", items);
+            var randomItemsWithFirst = testlet.Randomize();
+            var randomItemsItemsSecond = testlet.Randomize();
+            Assert.IsNotNull(randomItemsWithFirst);
+            Assert.IsNotNull(randomItemsItemsSecond);
+            Assert.IsTrue(randomItemsWithFirst.Count == 10);
+            Assert.IsTrue(randomItemsItemsSecond.Count == 10);
+            Assert.IsFalse(randomItemsWithFirst.SequenceEqual(randomItemsItemsSecond));
+        }
+    }
+}
