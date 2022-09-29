953f8143aad2e06e94bc0dafa73f01ff70cf4cfe
ravimanirajan <ravimanirajan@gmail.com>
2022-09-29 10:55:31 +0530

    Add project files.

---
+++ TestletConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWithTestlet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testlet Project!");
+
+
+            var testlet = new List<Item>() {
+                new Item(){ ItemId = "1", ItemType = ItemTypeEnum.Operational},
+                new Item(){ ItemId = "2", ItemType = ItemTypeEnum.Operational},
+                new Item(){ ItemId = "3", ItemType = ItemTypeEnum.Operational},
+                new Item(){ ItemId = "4", ItemType = ItemTypeEnum.Operational},
+                new Item(){ ItemId = "5", ItemType = ItemTypeEnum.Operational},
+                new Item(){ ItemId = "6", ItemType = ItemTypeEnum.Operational},
+                new Item(){ ItemId = "7", ItemType = ItemTypeEnum.Pretest},
+                new Item(){ ItemId = "8", ItemType = ItemTypeEnum.Pretest},
+                new Item(){ ItemId = "9", ItemType = ItemTypeEnum.Pretest},
+                new Item(){ ItemId = "10", ItemType = ItemTypeEnum.Pretest}
+            };
+            Testlet ob = new Testlet("1", testlet);
+            ob.Randomize();
+            Console.ReadLine();
+        }
+    }
+
+    public enum ItemTypeEnum
+    {
+        Pretest = 0,
+        Operational = 1
+    }
+}
---
+++ TestletConsoleApplication/Testlet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWithTestlet
+{
+    public class Item
+    {
+        public string ItemId { get; set; }
+        public ItemTypeEnum ItemType { get; set; }
+    }
+    public class Testlet : Item
+    {
+        public string TestletId;
+        private List<Item> Items;
+
+        public Testlet(string testletId, List<Item> items)
+        {
+            TestletId = testletId;
+            Items = items;
+        }
+        public List<Item> Randomize()
+        {            
+            List<Item> randomitems = new List<Item>();
+            if (Items != null && Items.Any())
+            {
+                var pretestItems = Items.Where(e => e.ItemType == ItemTypeEnum.Pretest).OrderBy(p => Guid.NewGuid()).Take(2).ToList();
+                randomitems.AddRange(pretestItems);
+
+                var allItems = Items.Where(e => !pretestItems.Any(p => p.ItemId == e.ItemId)).OrderBy(a => Guid.NewGuid());
+                randomitems.AddRange(allItems);
+            }
+            foreach (var item in randomitems)
+            {
+                Console.WriteLine(item.ItemType);
+            }
+            return randomitems;
+
+        }
+    }
+}
